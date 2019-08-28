5cb26838fe424e95476064766015dc1b1bde8909
shanmu2020 <854731306@QQ.com>
2019-08-28 17:41:03 +0800

    新增linux 版本

---
+++ src/linux.py
@@ -0,0 +1,48 @@
+from urllib.request import urlopen
+import json
+import subprocess, shlex
+import time
+import os
+
+
+def download(gitlabAddr, gitlabToken, ssh_or_http):
+    for index in range(10):
+        url = "%s/api/v4/projects?private_token=%s&per_page=100&page=%d&order_by=name" % (
+            gitlabAddr, gitlabToken, index)
+        print(url)
+        allProjects = urlopen(url)
+        allProjectsDict = json.loads(allProjects.read().decode())
+        if len(allProjectsDict) == 0:
+            break
+        for thisProject in allProjectsDict:
+            try:
+                if ssh_or_http == "ssh"
+                    # http下载
+                    thisProjectURL = thisProject['http_url_to_repo']
+                    thisProjectPath = thisProject['path_with_namespace']
+                    print(thisProjectURL + ' ' + thisProjectPath)
+                    if os.path.exists(thisProjectPath):
+                        command = shlex.split('git clone' % (thisProjectURL))
+                    else:
+                        command = shlex.split('git clone %s %s' % (thisProjectURL, thisProjectPath))
+                    resultCode = os.subprocess.Popen(command)
+                else:
+                    # ssh 下载
+                    thisProjectURL = thisProject['ssh_url_to_repo']
+                    thisProjectPath = thisProject['path_with_namespace']
+                    print(thisProjectURL + ' ' + thisProjectPath)
+                    if os.path.exists(thisProjectPath):
+                        command = shlex.split('git -C "%s" pull' % (thisProjectPath))
+                    else:
+                        command = shlex.split('git clone %s %s' % (thisProjectURL, thisProjectPath))
+
+                    resultCode = os.subprocess.Popen(command)
+                time.sleep(1)
+            except Exception as e:
+                print("Error on %s: %s" % (thisProjectURL, e.strerror))
+
+
+if __name__ == '__main__':
+    gitlabAddr = 'https://git.sensin-tech.cn'
+    gitlabToken = 'yNJQufxun5iCbzmjxmNW'
+    download(gitlabAddr, gitlabToken)
---
+++ src/win.py
@@ -0,0 +1,51 @@
+from urllib.request import urlopen
+import json
+import subprocess, shlex
+import time
+import os
+
+
+def download(gitlabAddr, gitlabToken, ssh_or_http):
+    for index in range(10):
+        url = "%s/api/v4/projects?private_token=%s&per_page=100&page=%d&order_by=name" % (
+            gitlabAddr, gitlabToken, index)
+        print(url)
+        allProjects = urlopen(url)
+        allProjectsDict = json.loads(allProjects.read().decode())
+        print(allProjectsDict)
+        if len(allProjectsDict) == 0:
+            print("###################")
+            print("未获取到任何项目")
+            print("###################")
+            break
+        for thisProject in allProjectsDict:
+            try:
+                if ssh_or_http == "ssh":
+                    # http下载
+                    thisProjectURL = thisProject['http_url_to_repo']
+                    thisProjectPath = thisProject['path_with_namespace']
+                    print(thisProjectURL + ' ' + thisProjectPath)
+                    if os.path.exists(thisProjectPath):
+                        command = 'git clone %s' % (thisProjectPath)
+                    else:
+                        command = 'git clone %s %s' % (thisProjectURL, thisProjectPath)
+                    resultCode = os.system(command)
+                else:
+                    # ssh 下载
+                    thisProjectURL = thisProject['ssh_url_to_repo']
+                    thisProjectPath = thisProject['path_with_namespace']
+                    print(thisProjectURL + ' ' + thisProjectPath)
+                    if os.path.exists(thisProjectPath):
+                        command = 'git -C "%s" pull' % (thisProjectPath)
+                    else:
+                        command = 'git clone %s %s' % (thisProjectURL, thisProjectPath)
+                    resultCode = os.system(command)
+                time.sleep(1)
+            except Exception as e:
+                print("Error on %s: %s" % (thisProjectURL, e.strerror))
+
+
+if __name__ == '__main__':
+    gitlabAddr = 'https://git.sensin-tech.cn'
+    gitlabToken = 'yNJQufxun5iCbzmjxmNW'
+    download(gitlabAddr, gitlabToken)
